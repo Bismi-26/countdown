5709156700188cd561ed1eaf89d41a9163bc671c
Bismi-26 <bismihameed26@gmail.com>
2025-05-23 09:34:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-setTimeout(() => {
-    document.getElementById("count").innerHTML = 10;
-    setTimeout(() => {
-        document.getElementById("count").innerHTML = 9;
-        setTimeout(() => {
-            document.getElementById("count").innerHTML = 8;
-            setTimeout(() => {
-                document.getElementById("count").innerHTML = 7;
-                setTimeout(() => {
-                    document.getElementById("count").innerHTML = 6;
-                    setTimeout(() => {
-                        document.getElementById("count").innerHTML = 5;
-                        setTimeout(() => {
-                            document.getElementById("count").innerHTML = 4;
-                            setTimeout(() => {
-                                document.getElementById("count").innerHTML = 3;
-                                setTimeout(() => {
-                                    document.getElementById("count").innerHTML = 2;
-                                    setTimeout(() => {
-                                        document.getElementById("count").innerHTML = 1;
-                                        setTimeout(() => {
-                                            document.getElementById("count").innerHTML = "Happy New Year!";
-                                        }, 1000);
-                                    }, 1000);
-                                }, 1000);
-                            }, 1000);
-                        }, 1000);
-                    }, 1000);
-                }, 1000);
-            }, 1000);
-        }, 1000);
-    }, 1000);
-}, 1000);
+import express from "express"
+import dotenv from"dotenv" 
+dotenv.config()
+import cors from"cors" 
+import connectDB from "./database/connectDB.js"
+import userRoutes from "./routes/userRoutes.js"
+
+const app= express()
+app.use(express.json())
+app.use(cors())
+app.use("/api/user",userRoutes)
+app.get("/",(req,res)=>{
+
+res.status(200).json({Message:"Api change sucessfully"})
+
+})
+app.get("/api/hear-beat",(req,res)=>{
+    req.status(200).json({data:"Heart beat was sucessfully"})
+    
+})
+
+app.listen(process.env.PORT,()=> {
+    console.log(`server running sucessfully  at port ${process.env.PORT}`)
+})
+connectDB();
